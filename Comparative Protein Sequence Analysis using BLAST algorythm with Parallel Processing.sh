8de930a40f6089916ec5a9ce2216216a1a400eeb
SzymonJak <126280450+SzymonJak982@users.noreply.github.com>
2023-10-26 11:47:34 +0200

    Comparative Protein Sequence Analysis using BLAST algorythm with Parallel Processing.sh

---
+++ Comparative Protein Sequence Analysis using BLAST algorythm with Parallel Processing.sh
@@ -0,0 +1,46 @@
+# Mini Project: Protein Sequence Analysis
+
+# setting the working directory 
+cd ./mini_project/mouse_protein_sequences
+
+# Download reference sequence for zebrafish
+curl -O ftp://ftp.ncbi.nih.gov/refseq/D_rerio/mRNA_Prot/zebrafish.1.protein.faa.gz
+
+# Similarly, download sequences 1 and 2 for the mouse
+curl -O ftp://ftp.ncbi.nih.gov/refseq/M_musculus/mRNA_Prot/mouse.1.protein.faa
+curl -O ftp://ftp.ncbi.nih.gov/refseq/M_musculus/mRNA_Prot/mouse.2.protein.faa
+
+# Count the number of sequences in the mouse1 and mouse2 files
+# Explanation: This uses grep to count the lines that start with ">" in the given files, indicating sequence headers.
+grep -c ">" mouse.1.protein.faa
+# Expected output: 42251
+grep -c ">" mouse.2.protein.faa
+# Expected output: 54783
+
+# Total number of sequences: 97034
+
+# Select the first 100 sequences using AWK
+# Explanation: AWK is used to filter and copy the first 100 sequences from the input file.
+awk '/^>/{n++} n<=100' mouse.1.protein.faa > mouse.1.protein.copy.faa
+
+# Create a BLAST database for the zebrafish protein sequences
+# Explanation: This command uses makeblastdb to create a BLAST database from the zebrafish protein sequences.
+makeblastdb -in zebrafish.1.protein.faa -dbtype prot
+
+# Perform BLAST analysis for mouse1 and mouse2 sequences against the zebrafish database
+# Explanation: BLAST is used to compare mouse protein sequences with zebrafish sequences and save the results.
+blastp -query mouse.1.protein.copy.faa -db zebrafish.1.protein.faa -out mouse1_output.txt
+blastp -query mouse.2.protein.faa -db zebrafish.1.protein.faa -out mouse2_output.txt
+
+# List of files for BLAST analysis:
+# mouse.1.protein.copy.faa
+# mouse.2.protein.faa
+
+# Measure the time needed for BLAST with specified parameters and HPC threads
+# Explanation: This command runs BLAST with the specified number of threads and records the execution time.
+# For 12 threads: 
+time blastp -query mouse.protein.faa.output -db zebrafish.1.protein.faa -num_threads 12 > out
+# For 4 threads: 
+time blastp -query mouse.protein.faa.output -db zebrafish.1.protein.faa -num_threads 4 > out
+
+# Expected output: BLAST results will be saved in the 'out' file, and the time taken will be measured.
